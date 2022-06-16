df0d843240c48609c8e9aa6d9c53d9bc4d2cbc58
trololzen <trololzen@gmail.com>
2022-06-16 15:17:01 +0200

    frontend is finished

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client';
 import Accordion from './Accordion/Accordion';
 import ScrollComponent from './ScrollComponent';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function MainComponent() {
 
     return (
-        <div>
+        <ParallaxProvider>
+
             <div>
                 <Accordion/>
             </div>
             <div className='main'>
            
             </div>
-        </div>
+            
+        </ParallaxProvider>
     )
 }
